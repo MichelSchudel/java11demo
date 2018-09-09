91092b36c50038daab5f9c713b1d24c9b91e1a3d
Schudel, MJ (Michel) <m.j.schudel@rn.rabobank.nl>
2018-09-09 14:51:13 +0200

    More demos

--- src/test/java/nl/craftsmen/demo/WebsocketDemo.java
+++ src/test/java/nl/craftsmen/demo/WebsocketDemo.java
@@ -1,15 +1,33 @@
 package nl.craftsmen.demo;
 
-import org.junit.Test;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
 
-import java.net.http.WebSocket;
+import static java.net.http.WebSocket.NORMAL_CLOSURE;
 
-public class WebsocketDemo {
+public class WebSocketDemo {
 
-    @Test
-    public void testWebsocket() {
-//        WebSocket.Builder.
-//        new WebSocket.Builder();
-//        WebSocket webSocket = WebSocket.Builder();
+    public static void main(String[] args) {
+        try {
+            var server = new WebSocketServer().getWebSocket();
+            server.sendPing(ByteBuffer.wrap("Ping: Client <--- Server".getBytes(Charset.forName("UTF-16"))));
+            server.sendPing(ByteBuffer.wrap("Pong: Client <--- Server".getBytes(Charset.forName("UTF-16"))));
+            server.sendText("Hello!", false);
+            server.sendClose(NORMAL_CLOSURE, "Goodbye!");
+        } catch (Exception e) {
+            System.out.println("Failure:" + e.getClass().toString().replace("class", "") + " was thrown.\nMessage: " + e.getMessage());
+
+            if (e.getMessage().contains("WebSocketHandshakeException")) {
+                var ex = ((java.net.http.WebSocketHandshakeException) e.getCause()).getResponse();
+                System.out.println("Body:\t" + ex.body());
+                System.out.println("Headers:");
+                ex.headers().map().forEach((k, v) -> System.out.println("\t" + k + ":  " + v));
+                System.out.println("HTTP request:  " + ex.request());
+                System.out.println("HTTP version:  " + ex.version());
+                System.out.println("Previous Reponse?:  " + ex.previousResponse());
+            }
+        }
     }
-}
+
+    ;
+};
